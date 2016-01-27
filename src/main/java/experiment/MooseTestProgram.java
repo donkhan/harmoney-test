ff71c81938eeac343a43710d160a65f0dd4bf1f7
donkhan <kamilkhan@rediffmail.com>
2016-01-27 08:29:24 +0530

    There is no thread here

--- src/main/java/experiment/MooseTestProgram.java
+++ src/main/java/experiment/MooseTestProgram.java
@@ -1,18 +1,35 @@
 package experiment;
 
+import java.io.IOException;
+
 
 
 public class MooseTestProgram {
 	
 	public static void main(String args[]){
-		String[] userNames = new String[]{"vteial"};
-		String[] passWords = new String[]{"1"};
+		String userName = "vteial";
+		String passWord = "1";
 		
-	
-		for(int i = 0;i<userNames.length;i++){
-			MooseTestThread mt = new MooseTestThread(userNames[i],passWords[i]);
-			mt.start();
+		LoginTest test = new LoginTest(userName,passWord);
+		try {
+			String sessionId = test.login();
+			MooseGETTest stest = new MooseGETTest(userName,sessionId);
+			stest.setURI("/harmoney2/get-stocks/Trichy/USD");
+			stest.execute();
+			
+			stest.setURI("/harmoney2/get-stocks/Trichy/USE");
+			stest.execute();
+			
+			stest.setURI("/harmoney2/get-stocks/Trichi/USD");
+			stest.execute();
+			
+			LogoutTest logoutTest = new LogoutTest(userName,sessionId);
+			logoutTest.execute();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	
 	}
 
 }
