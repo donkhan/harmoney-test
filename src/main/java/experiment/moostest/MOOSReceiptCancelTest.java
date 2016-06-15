d33e983d9a8f898372a4b3c451b7e9a98023b922
donkhan <=donkhan@yahoo.com>
2016-06-15 17:17:50 +0530

    Test code added for deleting moos transactions by receipt id

---
+++ src/main/java/experiment/moostest/MOOSReceiptCancelTest.java
@@ -0,0 +1,89 @@
+package experiment.moostest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.json.JSONObject;
+
+import experiment.authentication.LoginTest;
+import experiment.authentication.LogoutTest;
+import experiment.test.BaseTest;
+
+public class MOOSReceiptCancelTest extends BaseTest{
+	private String userName;
+	private String sessionId;
+	
+	public static void main(String args[]){
+		String userName = "feroz";
+		String passWord = "1";
+		
+		LoginTest loginTest = new LoginTest(userName,passWord);
+		String sessionId = "";
+		try {
+			sessionId = loginTest.login();
+			MOOSTransactionCancelTest ct = new MOOSTransactionCancelTest(userName,sessionId);
+			ct.executeTransactions();
+			
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} finally{
+			LogoutTest logout = new LogoutTest(userName,sessionId);
+			try {
+				logout.execute();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public MOOSReceiptCancelTest(String un,String sessionId){
+		this.userName = un;
+		this.sessionId = sessionId;
+	}
+	
+	@Override
+	public String getPayLoad() {
+		JSONObject obj =  new JSONObject();
+		obj.put("reason", "User cancelled Visit");
+		obj.put("branch-name","Trichy");
+		obj.put("receipt-id", 44);
+		System.out.println(obj.toString());
+		return obj.toString();
+	}
+
+	@Override
+	public String getURI() {
+		return "/harmoney2/moos/cancel-transaction-by-receipt"; 
+	}
+
+	@Override
+	public void addExtraHeaders(HttpPost request) {
+		request.addHeader("X-userId",userName);
+		request.addHeader("Cookie","JSESSIONID="+sessionId);
+	}
+	
+	public void executeTransactions() throws ClientProtocolException, IOException{
+		HttpResponse response = super.execute();
+		System.out.println("Response Code : " 
+	                + response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+			new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		System.out.println(result);
+
+	}
+
+
+}
